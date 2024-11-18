d850a72fbf5e4be7c72df85d7c0ac1c287b4c09a
Singul0 <127663818+Singul0@users.noreply.github.com>
2024-11-18 20:14:07 +0700

    Calculator V2.0, tons of new features TODO: Polish and comments

--- Arduino_Calculator/Arduino_Code.ino
+++ Arduino_Calculator/Arduino_Code.ino
@@ -8,24 +8,49 @@
 #include <Keypad.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); 
-const byte ROWS = 4;
-const byte COLS = 4;
 
-char keys [ROWS] [COLS] = {
+//Keypad 4x4 Big
+const byte BIG_ROWS = 4;
+const byte BIG_COLS = 4;
+
+char keys [BIG_ROWS] [BIG_COLS] = {
   {'1', '2', '3', '/'},
   {'4', '5', '6', '*'},
   {'7', '8', '9', '-'},
-  {'C', '0', '=', '+'}
+  {'E', '0', '=', '+'}
 };
-byte rowPins[ROWS] = { 0, 1, 2, 3 };// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
-byte colPins[COLS] = { 9, 6, 5, 4 }; // Connect keypad COL0, COL1 and COL2 to these Arduino pins.
 
-Keypad myKeypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+char keys_advanced [BIG_ROWS] [BIG_COLS] = {
+  {'1', '2', '3', 'S'},
+  {'4', '5', '6', 'C'},
+  {'7', '8', '9', 'T'},
+  {'E', '0', '=', 'R'}
+};
 
+byte big_rowPins[BIG_ROWS] = { 0, 1, 2, 3 };// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
+byte big_colPins[BIG_COLS] = { 4, 5, 6, 7 }; // Connect keypad COL0, COL1 and COL2 to these Arduino pins.
+
+Keypad normal_keypad = Keypad( makeKeymap(keys), big_rowPins, big_colPins, BIG_ROWS, BIG_COLS);
+Keypad advanced_keypad = Keypad( makeKeymap(keys_advanced), big_rowPins, big_colPins, BIG_ROWS, BIG_COLS);
+
+// Keypad 1x4 Small
+const byte ROWS = 1;
+const byte COLS = 4;
+
+char small_keys[ROWS][COLS] = {
+    {'A','B','C','D'}
+};
+
+byte rowPins[ROWS] = {13};// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
+byte colPins[COLS] = {9,10,11,12}; // Connect keypad COL0, COL1 and COL2 to these Arduino pins.
+
+Keypad mySmallKeypad = Keypad( makeKeymap(small_keys), rowPins, colPins, ROWS, COLS );
 
 boolean presentValue = false; // variable ini menyatakan jika num1 sudah diberi op (operator). digunakan agar menyatakan input adalah untuk num1 atau num2
 boolean is_continuing_from_prev_calculation = false;
 boolean final = false;
+boolean backlight_on;
+boolean advanced_mode = false;
 String calculator_mode = "calculator"; //Mode kalkulator atau game (ide game pending)? TODO: IMPLEMENT GAME AND MODE CHANGES
 String num1, num2; // num1 (angka yang dioperasikan dengan num2) num2 (angka yang dioperasikan dengan num2)
 int clear_or_reset_counter = 0; //seberapa banyaknya kalkulator sudah direset (buat mengganti mode operasi kalkulator TODO:rencananya)
@@ -38,7 +63,7 @@ void intro_display_text(String text_above, String text_below)
   lcd.print(text_above);
   lcd.setCursor(0, 1);
   lcd.print(text_below);
-  delay(5000);
+  delay(2000);
   lcd.clear();
 }
 
@@ -69,18 +94,20 @@ void calculator_actual_mode(char key)
     }
   }
 
-  else if (presentValue == false && key != NO_KEY && (key == '/' || key == '*' || key == '-' || key == '+')) //Pemilihan operator
+  else if (presentValue == false && key != NO_KEY && (key == '/' || key == '*' || key == '-' || key == '+' || key == 'S' || key == 'C' || key == 'T' || key == 'R')) //Pemilihan operator
   {
-    if (presentValue == false)
-    {
-      presentValue = true;
-      op = key;
-      lcd.setCursor(15,0);
-      lcd.print(op);
-    }
+    presentValue = true;
+    op = key;
+    lcd.setCursor(15,0);
+    lcd.print(op);
+
+    if(key == 'S' || key == 'C' || key == 'T' || key == 'R')
+      final = true;
   }
 
-  else if (final == true && key != NO_KEY && key == '='){ 
+  else if (final == true && key != NO_KEY && key == '='){
+    float float_number;
+
     if (op == '+'){
       answer = num1.toDouble() + num2.toDouble();
     }
@@ -92,7 +119,26 @@ void calculator_actual_mode(char key)
     }
     else if (op == '/'){
       answer = num1.toDouble() / num2.toDouble();
-    }    
+    }
+    else if (op == 'S'){
+      float_number = num1.toFloat();
+      float_number = sin(float_number);
+      answer = float_number;
+      
+    }
+    else if (op == 'C'){
+      float_number = num1.toFloat();
+      float_number = cos(float_number);
+      answer = float_number;
+    }
+    else if (op == 'T'){
+      float_number = num1.toFloat();
+      float_number = tan(float_number);
+      answer = float_number;
+    }
+    else if (op == 'R'){
+      answer = (num1.toDouble() * 71) / 4068;
+    }   
       lcd.clear();
       lcd.print(answer);
       presentValue = false;
@@ -104,23 +150,53 @@ void calculator_actual_mode(char key)
       op = ' ';
 
   }
-  else if (key != NO_KEY && key == 'C'){ //reset menjadi semula
+  else if (key != NO_KEY && key == 'E'){ //reset menjadi semula
     lcd.clear();
     presentValue = false;
     final = false;
     num1 = "";
     num2 = "";
+    is_continuing_from_prev_calculation = false;
     answer = 0;
     op = ' ';
     clear_or_reset_counter = clear_or_reset_counter + 1; //sebenernya bisa si, cuman clear_or_reset_counter++ tapi susah di baca imo -_-
   }
 }
 
+void button_keys(char button)
+{
+  if(button != NO_KEY && (button=='A'||button=='B'||button=='C'||button=='D')) {
+    lcd.clear();
+    if(button == 'A') {
+      intro_display_text("Toggle Operasi", "Tingkat Lanjut");
+      advanced_mode = !advanced_mode;
+    }
+    if(button == 'B') {
+      intro_display_text("KAMU PENCET", "B");
+    }
+    if(button == 'C') {
+      intro_display_text("KAMU PENCET", "C");
+    }
+    if(button == 'D') {
+      if (backlight_on){
+        intro_display_text("Mematikan", "Kalkulator...");
+        lcd.noBacklight();
+        backlight_on = false;
+      }
+      else {
+        lcd.backlight();
+        intro_display_text("Menyalakan", "Kalkulator...");
+        backlight_on = true;
+      }
+    }
+  }
+}
 
 void setup()
 {
   lcd.init();         // initialize the lcd
   lcd.backlight();    // Turn on the LCD screen backlight
+  backlight_on = true;
   intro_display_text("P5 Kelompok 1 XI.1", "Kalkulator");
   intro_display_text("Berdasarkan", "Arduino");
   delay(5000);
@@ -129,12 +205,14 @@ void setup()
 
 void loop()
 {
-  char key = myKeypad.getKey();
-  if(calculator_mode == "calculator")
-  {
-    calculator_actual_mode(key);
+  char key;
+  if(advanced_mode) {
+    key = advanced_keypad.getKey();  
+  } else {
+    key = normal_keypad.getKey();
   }
-  else{
-    calculator_actual_mode(key); //TODO: FUNCTION FOR COWS AND BULLS GAMEMODE
-  }
+
+  char button = mySmallKeypad.getKey();
+  button_keys(button);
+  calculator_actual_mode(key);
 }
